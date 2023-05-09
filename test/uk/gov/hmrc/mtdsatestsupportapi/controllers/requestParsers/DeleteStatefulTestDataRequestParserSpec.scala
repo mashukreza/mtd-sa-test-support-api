9c0e40fb408f14b503ed941e7210549b6a047dc9
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-05-09 12:07:23 +0100

    MTDSA-16551: Addressing pr reviewer comments

--- test/uk/gov/hmrc/mtdsatestsupportapi/controllers/requestParsers/DeleteStatefulTestDataRequestParserSpec.scala
+++ test/uk/gov/hmrc/mtdsatestsupportapi/controllers/requestParsers/DeleteStatefulTestDataRequestParserSpec.scala
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.mtdsatestsupportapi.controllers.requestParsers
 
-import api.models.errors.{ErrorWrapper, VendorClientIdFormatError}
+import api.models.errors.{ErrorWrapper, NinoFormatError}
 import play.api.libs.json.{JsObject, JsValue, Json}
 import uk.gov.hmrc.mtdsatestsupportapi.helpers.UnitSpec
 import uk.gov.hmrc.mtdsatestsupportapi.mocks.validators.MockDeleteStatefulTestDataValidator
-import uk.gov.hmrc.mtdsatestsupportapi.models.domain.VendorClientId
 import uk.gov.hmrc.mtdsatestsupportapi.models.request.deleteStatefulTestData.{DeleteStatefulTestDataRawData, DeleteStatefulTestDataRequest}
 
 class DeleteStatefulTestDataRequestParserSpec extends UnitSpec {
@@ -19,14 +34,14 @@ class DeleteStatefulTestDataRequestParserSpec extends UnitSpec {
       "valid request data is supplied" in new Test {
         MockDeleteStatefulTestDataValidator.validate(data).returns(Nil)
 
-        parser.parseRequest(data) shouldBe Right(DeleteStatefulTestDataRequest(VendorClientId(vendorClientId), Some(requestBody)))
+        parser.parseRequest(data) shouldBe Right(DeleteStatefulTestDataRequest(vendorClientId, Some(requestBody)))
       }
     }
     "return an error" when {
       "a single validation error occurs" in new Test {
-        MockDeleteStatefulTestDataValidator.validate(data).returns(List(VendorClientIdFormatError))
+        MockDeleteStatefulTestDataValidator.validate(data).returns(List(NinoFormatError))
 
-        parser.parseRequest(data) shouldBe Left(ErrorWrapper(correlationId, VendorClientIdFormatError, None))
+        parser.parseRequest(data) shouldBe Left(ErrorWrapper(correlationId, NinoFormatError, None))
       }
     }
   }
